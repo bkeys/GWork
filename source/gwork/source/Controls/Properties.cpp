1c5a2736fc4f0e58545e20ea727884aaabef3708
Brigham Henry Keys, Esq <bkeys@bkeys.org>
2023-10-12 10:01:47 -0400

    Added ability to set splitter widths

--- source/gwork/source/Controls/Properties.cpp
+++ source/gwork/source/Controls/Properties.cpp
@@ -37,6 +37,12 @@ void Properties::OnSplitterMoved(Event::Info)
     InvalidateChildren();
 }
 
+void SetSplitterWidth(const unsigned int x_) {
+  Gwk::Rect current_bounds = m_splitterBar->GetBounds();
+  current_bounds.x = x_;
+  m_splitterBar->SetBounds(current_bounds);
+}
+
 int Properties::GetSplitWidth()
 {
     return m_splitterBar->X();
